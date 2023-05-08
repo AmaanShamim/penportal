6b3bf951ed98bad0a06679f9faa9c7b860fac4f3
Amaan <amaans1912@gmail.com>
2023-05-08 05:36:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 style={{ fontFamily: 'MontserratAlt1-ExtraBold', fontSize: '32px' }}>PenPortal</h1>
+    </>
+  );
+}
+
+export default App;
